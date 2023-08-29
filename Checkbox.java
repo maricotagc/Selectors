6e88d5579b8d3722a6736b6bfffbc114f3d0367a
maricotagc <36520845+maricotagc@users.noreply.github.com>
2023-08-29 21:00:06 +0200

    Update Checkbox.java

--- Checkbox.java
+++ Checkbox.java
@@ -24,6 +24,10 @@ public class Dropdown extends Base
 
     @Test
     public void CheckboxSelectionTest(){
+        //Given
+        String expectedDisplayedInput = "Subscription: Subscribed";
+
+        //When
         // Find the checkbox element
         WebElement checkbox = driver.findElement(By.id("subscribe"));
 
@@ -34,49 +38,13 @@ public class Dropdown extends Base
         // Click the checkbox to toggle its state
         checkbox.click();
 
+        //Click on Submit button
+        driver.findElement(By.cssSelector("input[type='button'][value='Display Input'][onclick='displayInput()']")).click();
+        String actualDisplayedInput = driver.findElement(By.id("display-subscribe")).getText();
+
         // Check the updated state of the checkbox
         boolean isCheckedAfterClick = checkbox.isSelected();
         Assertions.assertTrue(isCheckedAfterClick, "Checkbox should be checked after click");
-        System.out.println("");
-    }
-
-    @Test
-    public void TextInputTest() {
-        //Given
-        String expectedValue = "Izabela Moita";
-
-        //When
-        // Find the text input field element
-        WebElement inputField = driver.findElement(By.id("name"));
-
-        // Enter a value into the input field
-        String inputValue = "Izabela Moita";
-        inputField.sendKeys(inputValue);
-        // Get the current value of the input field
-        String actualValue = inputField.getAttribute("value");
-
-        //Then
-        // Verify that the value matches the expected input
-        Assertions.assertEquals(expectedValue, actualValue);
-    }
-
-    @Test
-    public void testTextareaContentAssertion() {
-        //Given
-        String expectedResult = "I am the most beautiful baby in the world";
-
-        //When
-        // Find the textarea element
-        WebElement textarea = driver.findElement(By.id("comments"));
-
-        // Enter content into the textarea
-        String inputContent = "I am the most beautiful baby in the world";
-        textarea.sendKeys(inputContent);
-
-        // Get the current content of the textarea
-        String actualContent = textarea.getAttribute("value");
-
-        // Verify that the content matches the expected input
-        Assertions.assertEquals(expectedResult, actualContent);
+        Assertions.assertEquals(expectedDisplayedInput, actualDisplayedInput);
     }
 }
