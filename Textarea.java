8c492d3647d3fde6fe2ceb084a7e6c63d101021e
maricotagc <36520845+maricotagc@users.noreply.github.com>
2023-08-29 20:27:21 +0200

    Create Textarea.java

---
+++ Textarea.java
@@ -0,0 +1,44 @@
+package moita;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Dropdown extends Base
+{
+    private Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+    @BeforeEach
+    public void openMainPage() {
+        driver.get(BASE_URL);
+        driver.findElement(By.cssSelector("#details-button")).click();
+        driver.findElement(By.cssSelector("#proceed-link")).click();
+    }
+
+    @Test
+    public void testTextareaContentAssertion() {
+        //Given
+        String expectedResult = "I am the most beautiful baby in the world";
+
+        //When
+        // Find the textarea element
+        WebElement textarea = driver.findElement(By.id("comments"));
+
+        // Enter content into the textarea
+        String inputContent = "I am the most beautiful baby in the world";
+        textarea.sendKeys(inputContent);
+
+        // Get the current content of the textarea
+        String actualContent = textarea.getAttribute("value");
+
+        // Verify that the content matches the expected input
+        Assertions.assertEquals(expectedResult, actualContent);
+    }
+}
