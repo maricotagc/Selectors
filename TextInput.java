ef801d128561ce8f7efde069ef769dad6b1be462
maricotagc <36520845+maricotagc@users.noreply.github.com>
2023-08-30 10:55:46 +0200

    Update TextInput.java

--- TextInput.java
+++ TextInput.java
@@ -22,7 +22,7 @@ public class Dropdown extends Base
         driver.findElement(By.cssSelector("#proceed-link")).click();
     }
 
-    @Test
+   @Test
     public void TextInputTest() {
         //Given
         String expectedValue = "Izabela Moita";
