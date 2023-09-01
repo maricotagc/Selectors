5d14f4c1f6a8b404ee5c022f7fdfb94ef25982bc
maricotagc <36520845+maricotagc@users.noreply.github.com>
2023-09-01 21:42:54 +0200

    Update Iframe.java

--- Iframe.java
+++ Iframe.java
@@ -19,24 +19,25 @@ public class TechlisticTest
         //When
         seleniumUtil.openPage("https://www.techlistic.com/p/selenium-practice-form.html");
         seleniumUtil.clickButtonByCss("#ez-accept-all");
-        WebDriver frame1Driver = driver.switchTo().frame("google_ads_iframe_/1254144,22662382379/techlistic_com-pixel1_0");
-        WebDriver frame2Driver = frame1Driver.switchTo().frame("ad_iframe");
+        WebDriver frame1Driver = seleniumUtil.changeIframeById("google_ads_iframe_/1254144,22662382379/techlistic_com-pixel1_0");
+        WebDriver frame2Driver = seleniumUtil.changeIframeById(frame1Driver,"ad_iframe");
+
         List<WebElement> spans = frame2Driver.findElements(By.tagName("span"));
 
         for (WebElement span : spans) {
-            System.out.println("Oi" +span.getText());
             if (span.getText().equals("Close")) {
                 span.click();
                 break;
             }
         }
+        driver.switchTo().parentFrame();
+        seleniumUtil.clickButtonById("#ezmob-footer-close");
+        seleniumUtil.clickButtonById("#cookieChoiceDismiss");
+        seleniumUtil.writeInputTextByCss("Chun-Li","[name='firstname']");
+        seleniumUtil.writeInputTextByCss("Moita", "[name='lastname']");
 
-        seleniumUtil.clickButtonByCss(".ns-nnbzd-e-16");
-        seleniumUtil.writeInputTextByCss("Chun-Li","firstname");
-        seleniumUtil.writeInputTextByCss("Moita", "lastname");
-
-        String actualFirstName = seleniumUtil.readInputTextValueByCss("firstname");
-        String actualLastName = seleniumUtil.readInputTextValueByCss("lastname");
+        String actualFirstName = seleniumUtil.readInputTextValueByCss("[name='firstname']");
+        String actualLastName = seleniumUtil.readInputTextValueByCss("[name='lastname']");
 
         //Then
         Assertions.assertEquals(expectedFirstName, actualFirstName);
